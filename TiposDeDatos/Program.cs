943309f04ea5a5a5f859cf6c61b21dfc98b49941
Erick Espinoza <eespinozaa0489@uvirtual.ac.cr>
2023-09-20 20:59:06 -0600

    Add project files.

---
+++ TiposDeDatos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiposDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            Console.WriteLine("Digite su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el monto");
+            float monto = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los meses");
+            float meses = float.Parse(Console.ReadLine());
+            float cuota = monto / meses;
+            float total = cuota * 1.05f;
+            Console.WriteLine($"Por mes debe pagar {cuota}");
+
+
+
+        }
+    }
+}
